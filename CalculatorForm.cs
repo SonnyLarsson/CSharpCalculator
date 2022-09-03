b3ab5ffe6afdbcd3623c4c10408e033d2856272e
Sonny Larsson <sonny.larsson@gmail.com>
2022-09-03 15:08:27 -0500

    Some cleanup and tiny optimization.

--- CalculatorForm.cs
+++ CalculatorForm.cs
@@ -7,11 +7,9 @@ namespace CSharpCalculator
 {
     public partial class CalculatorForm : Form
     {
-        private CalcButtonMethods calc;
-        private ICalcResult calcResult;      
-        private string currentNumber;
-        private string sumString;
-        private string previousSumString;
+        private CalcButtonMethods _calc;
+        private ICalcResult _calcResult;
+        private string _previousSumString;
 
         public CalculatorForm()
         {
@@ -26,13 +24,15 @@ namespace CSharpCalculator
             };
 
             CalcNumber.Text = calculator.Number.ToString();
-            previousSumString = "0";
+            _previousSumString = "0";
             ButtonDecimalSeparator.Text = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
             CurrentSumLabel.Text = "0";
             ResultLabel.Text = "";
 
-            calc = new CalcButtonMethods(calculator);
+            _calc = new CalcButtonMethods(calculator);
         }
+
+        #region CalcNumber
 
         private void CalcNumber_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -47,13 +47,17 @@ namespace CSharpCalculator
                 CalcNumber.SelectionStart = CalcNumber.Text.Length;
                 CalcNumber.SelectionLength = 0;
             }
-            if (CalcNumber.Text.StartsWith(",")) CalcNumber.Text = "0" + CalcNumber.Text ;
+            if (CalcNumber.Text.StartsWith(",")) CalcNumber.Text = "0" + CalcNumber.Text;
         }
 
         private void CalcNumber_Click(object sender, EventArgs e)
         {
             if (CalcNumber.Text == "0") CalcNumber.Text = "";
         }
+
+        #endregion
+
+        #region calc_buttons
 
         private void AddButton_Click(object sender, EventArgs e)
         {
@@ -75,47 +79,122 @@ namespace CSharpCalculator
             ButtonAction(ECalcButtonActions.Divide);
         }
 
+        #endregion
+
+        #region calc_button_actions
+
         private void ButtonAction(ECalcButtonActions currentAction)
         {
-            currentNumber = CalcNumber.Text;
             ResolveOperation(currentAction);
             CalcNumber.Text = "0";
         }
 
         private void ResolveOperation(ECalcButtonActions currentAction)
         {
-            previousSumString = calcResult == null ? "0" : calcResult.NumberString;
+            _previousSumString = _calcResult == null ? "0" : _calcResult.NumberString;
             switch (currentAction)
             {
-                case ECalcButtonActions.Equals:
-                    sumString = calcResult.NumberString;
-                    break;
                 case ECalcButtonActions.Add:
-                    calcResult = calc.Add(CalcNumber.Text);
-                    ResolveInput(calcResult.NumberString, "+");
+                    _calcResult = _calc.Add(CalcNumber.Text);
+                    ResolveInput(_calcResult.NumberString, "+");
                     break;
                 case ECalcButtonActions.Subtract:
-                    calcResult = calc.Subtract(CalcNumber.Text);
-                    ResolveInput(calcResult.NumberString, "-");
+                    _calcResult = _calc.Subtract(CalcNumber.Text);
+                    ResolveInput(_calcResult.NumberString, "-");
                     break;
                 case ECalcButtonActions.Multiply:
-                    calcResult = calc.Multiply(CalcNumber.Text);
-                    ResolveInput(calcResult.NumberString, "*");
+                    _calcResult = _calc.Multiply(CalcNumber.Text);
+                    ResolveInput(_calcResult.NumberString, "*");
                     break;
                 case ECalcButtonActions.Divide:
-                    calcResult = calc.Divide(CalcNumber.Text);
-                    ResolveInput(calcResult.NumberString, "/");
+                    _calcResult = _calc.Divide(CalcNumber.Text);
+                    ResolveInput(_calcResult.NumberString, "/");
                     break;
                 default:
                     break;
             }
         }
 
+        #endregion
+
+        #region input_buttons
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("1");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("2");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("3");
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("4");
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("5");
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("6");
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("7");
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("8");
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("9");
+        }
+
+        private void ButtonDecimalSeparator_Click(object sender, EventArgs e)
+        {
+            if (CalcNumber.Text == "") CalcNumber.Text = "0";
+
+            if (!CalcNumber.Text.Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator))
+            {
+                CalcNumber.Text += CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            }
+
+        }
+
+        private void Button0_Click(object sender, EventArgs e)
+        {
+            AddCalcNumber("0");
+        }
+
+        private void ButtonC_Click(object sender, EventArgs e)
+        {
+            _calcResult = _calc.Clear();
+            CalcNumber.Text = _calcResult.NumberString;
+            CurrentSumLabel.Text = _calcResult.NumberString;
+            ResultLabel.Text = _calcResult.Info;
+        }
+
+        #endregion
+
         private void ResolveInput(string sumString, string operation)
-        { 
+        {
             if (CalcNumber.Text != "")
             {
-                var operationString = $"{previousSumString} {operation} {CalcNumber.Text}";
+                var operationString = $"{_previousSumString} {operation} {CalcNumber.Text}";
                 ResultLabel.Text = $"{operationString} = {sumString}";
                 CurrentSumLabel.Text = sumString;
             }
@@ -130,86 +209,13 @@ namespace CSharpCalculator
             return true;
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void AddCalcNumber(string numberString)
         {
-            ClearNumberZero();
-            CalcNumber.Text += "1";
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "2";
-        }
-
-        private void Button3_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "3";
-        }
-
-        private void Button4_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "4";
-        }
-
-        private void Button5_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "5";
-        }
-
-        private void Button6_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "6";
-        }
-
-        private void Button7_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "7";
-        }
-
-        private void Button8_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "8";
-        }
-
-        private void Button9_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "9";
-        }
-
-        private void ButtonDecimalSeparator_Click(object sender, EventArgs e)
-        {
-            if (CalcNumber.Text == "") CalcNumber.Text = "0";
-
-            if (!CalcNumber.Text.Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)) {
-                CalcNumber.Text += CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
-            }
-            
-        }
-
-        private void Button0_Click(object sender, EventArgs e)
-        {
-            ClearNumberZero();
-            CalcNumber.Text += "0";
-        }
-
-        private void ButtonC_Click(object sender, EventArgs e)
-        {
-            calcResult = calc.Clear();
-            CalcNumber.Text = calcResult.NumberString;
-            CurrentSumLabel.Text = calcResult.NumberString;
-            ResultLabel.Text = calcResult.Info;
-        }
-
-        private void ClearNumberZero() {
-            if (CalcNumber.Text == "0") CalcNumber.Text = "";
+            if (CalcNumber.Text == "0") {
+                CalcNumber.Text = numberString;
+            } else {
+                CalcNumber.Text += numberString;
+            }            
         }
     }
 }
